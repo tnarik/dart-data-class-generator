513775534dd4793d6bd36f33ed8206614e7b3139
tnarik <tnarik@lecafeautomatique.co.uk>
2021-07-25 08:45:02 +0100

    Added change-case integration and a test, plus template selection for generateDataClassFromJsonWithTemplate

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -23,7 +23,7 @@ const {
     getReplaceEdit,
 } = require('../editor_helpers');
 
-// const {camelCase} = require('change-case'); // This introduces the largest penalty in startup time (10 ms, worth checking to understand it better)
+const {camelCase} = require('change-case'); // This introduces the largest penalty in startup time (10 ms, worth checking to understand it better)
 
 /**
  * @param {DartClass[]} theClasses
@@ -54,6 +54,7 @@ const {
  * @param {string} projectName
  */
 async function generateDataClass(isFlutter, projectName, text = getDocText()) {
+  console.log(`asdf ${camelCase('arriba la vida')}`)
     if (getLangId() == 'dart') {
         const reader = new DartClassReader(text, null, projectName);
         const generator = new DataClassGenerator(reader.theClasses, reader.imports,false, isFlutter, projectName);
@@ -167,57 +168,96 @@ async function generateDataClassFromJson(isFlutter, projectName) {
  * @param {string} projectName
  */
  async function generateDataClassFromJsonWithTemplate(isFlutter, projectName) {
-    let langId = getLangId();
-    if (langId == 'dart') {
-        let documentText = getDocText();
-
-        const name = await vscode.window.showInputBox({
-            placeHolder: 'Please type in a class name.'
-        });
-
-        if (name == null || name.length == 0) {
-            return;
-        }
-
-        let reader = new JsonReader(isFlutter, projectName, documentText, name);
-        let separate = true;
-
-        if (await reader.error == null) {
-            if (reader.files.length >= 2) {
-                const setting = readSetting('json.separate');
-                if (setting == 'ask') {
-                    const r = await vscode.window.showQuickPick(['Yes', 'No'], {
-                        canPickMany: false,
-                        placeHolder: 'Do you wish to separate the JSON into multiple files?'
-                    });
-
-                    if (r != null) {
-                        separate = r == 'Yes';
-                    } else {
-                        return;
-                    }
-                } else {
-                    separate = (setting == 'separate');
-                }
-            }
-
-            vscode.window.withProgress({
-                location: vscode.ProgressLocation.Notification,
-                cancellable: false
-            }, async function (progress, token) {
-                progress.report({ increment: 0, message: 'Generating Data Classes...' });
-                scrollTo(0);
-                await reader.commitJson(progress, separate);
-                clearSelection();
-            });
-        } else {
-            showError(await reader.error);
-        }
-    } else if (langId == 'json') {
-        showError('Please paste the JSON directly into an empty .dart file and then try again!');
-    } else {
-        showError('Make sure that you\'re editing a dart file and then try again!');
+    // If there are templates, then go ahead
+    let templates = readSetting('templates') || [];
+    if (!templates.length) {
+      return showError("No configured templates found!");
     }
+
+    // Select a template
+    let selectedTemplate = null;
+    if (templates.length == 1) {
+      selectedTemplate = templates[0];
+
+      const response = await vscode.window.showQuickPick(['Yes', 'No'], {
+          canPickMany: false,
+          placeHolder: `Template '${selectedTemplate.name}' selected automatically. Do you want to proceed?`
+      });
+
+      if (response != null) {
+          if (response == 'No') {
+              return showInfo('Class generation cancelled');
+          }
+      } else {
+          return;
+      }
+    } else {
+      let selectedTemplateName = await vscode.window.showQuickPick(
+        templates.map((template) => template.name), {
+            canPickMany: false,
+            placeHolder: `Please select the template you want to use for class generation`
+        }
+      );
+      selectedTemplate = templates.find(temp => temp.name === selectedTemplateName);
+    }
+
+    console.warn(`Selected template ${selectedTemplate.name}`)
+    console.log(selectedTemplate)
+
+    // With the template selected, we are ready to proceed (parseJSON -> apply template)
+
+
+    // let langId = getLangId();
+    // if (langId == 'dart') {
+    //     let documentText = getDocText();
+
+    //     const name = await vscode.window.showInputBox({
+    //         placeHolder: 'Please type in a class name.'
+    //     });
+
+    //     if (name == null || name.length == 0) {
+    //         return;
+    //     }
+
+    //     let reader = new JsonReader(isFlutter, projectName, documentText, name);
+    //     let separate = true;
+
+    //     if (await reader.error == null) {
+    //         if (reader.files.length >= 2) {
+    //             const setting = readSetting('json.separate');
+    //             if (setting == 'ask') {
+    //                 const r = await vscode.window.showQuickPick(['Yes', 'No'], {
+    //                     canPickMany: false,
+    //                     placeHolder: 'Do you wish to separate the JSON into multiple files?'
+    //                 });
+
+    //                 if (r != null) {
+    //                     separate = r == 'Yes';
+    //                 } else {
+    //                     return;
+    //                 }
+    //             } else {
+    //                 separate = (setting == 'separate');
+    //             }
+    //         }
+
+    //         vscode.window.withProgress({
+    //             location: vscode.ProgressLocation.Notification,
+    //             cancellable: false
+    //         }, async function (progress, token) {
+    //             progress.report({ increment: 0, message: 'Generating Data Classes...' });
+    //             scrollTo(0);
+    //             await reader.commitJson(progress, separate);
+    //             clearSelection();
+    //         });
+    //     } else {
+    //         showError(await reader.error);
+    //     }
+    // } else if (langId == 'json') {
+    //     showError('Please paste the JSON directly into an empty .dart file and then try again!');
+    // } else {
+    //     showError('Make sure that you\'re editing a dart file and then try again!');
+    // }
 }
 
 module.exports = {
